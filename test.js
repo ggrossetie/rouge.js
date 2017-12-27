ab5e7caafc75075ca2f0d4ef6919c5a37818f937
Guillaume Grossetie <ggrossetie@gmail.com>
2017-12-27 17:22:41 +0100

    Initialize project

---
+++ test.js
@@ -0,0 +1,11 @@
+Opal = require('opal-runtime').Opal;
+
+Opal.require('nodejs');
+
+require('./build/rouge-lib.js');
+//Opal.require('rouge');
+
+//console.log(Opal.Rouge);
+//Prawn::Document.generate("hello.pdf") do
+//  text "Hello World!"
+//end
